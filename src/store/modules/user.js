f96f1ea45377dd04df52a11e7d2a01bd72f708c7
huanggz <918031078@qq.com>
2021-01-27 15:29:07 +0800

    编辑个人资料

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,7 +2,7 @@ import wepy from '@wepy/core'
 import { login, logout, refresh, register } from '@/api/auth'
 import * as auth from '@/utils/auth'
 import isEmpty from 'lodash/isEmpty'
-import { getCurrentUser } from '@/api/user'
+import { getCurrentUser, updateUser } from '@/api/user'
 
 const getDefaultState = () => {
   return {
@@ -72,8 +72,15 @@ const actions = {
     // 清空 storage
     auth.logout()
     commit('resetState')
-  }
+  },
 
+  async updateUser ({ commit }, params = {}) {
+
+    const editResponse = await updateUser(params)
+
+    commit('setUser', editResponse.data)
+    auth.setUser(editResponse.data)
+  }
 
 }
 
