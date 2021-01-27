90a93a62f59db4c8cd6dfce4fba7422f5e3fc4c3
huanggz <918031078@qq.com>
2021-01-27 15:49:19 +0800

    上传头像

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,4 @@
-import { request, authRequest } from '@/utils/request'
+import { request, authRequest, uploadFile } from '@/utils/request'
 
 export function getCurrentUser(data) {
   return authRequest('user')
@@ -8,5 +8,16 @@ export function updateUser(data) {
   return authRequest('user', {
     method: 'put',
     data: data
+  })
+}
+
+export function updateAvatar(avatar) {
+  return uploadFile('images', {
+    method: 'post',
+    name: 'image',
+    formData: {
+      type: 'avatar'
+    },
+    filePath: avatar
   })
 }
